331749b29fb6c2d107a1a0e50fe3af75a37ed5e6
t-creates <geislinger@pm.me>
2022-10-12 11:09:58 -0600

    components

---
+++ components/PasswordForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PasswordForm = () => {
+  return (
+    <div>
+      <h1>Password Genorator</h1>
+    </div>
+  )
+}
+
+export default PasswordForm
---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from './Button'
+export { default as Checkbox } from './Checkbox'
+export { default as PasswordForm } from './PasswordForm'
+export { default as StrengthBar } from './StrengthBar'
+export { default as Slider } from './Slider'
+export { default as Password } from './Password'
